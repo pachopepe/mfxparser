38ff751d73edc62e5adea01448489466d67a4485
Francisco José Cháves <pachopepe@gmail.com>
2015-04-06 16:18:10 -0500

    Initial load

--- src/MixFix/Parser.hs
+++ src/MixFix/Parser.hs
@@ -2,7 +2,8 @@
 
 -- Francisco Cháves
 --
--- This is a mixfix parser work on with a calculational expressions for quantifiers and containers 
+-- This is a mixfix parser work on.
+-- I included  calculational expressions for quantifiers, containers and substitution
 -- (http://www.cs.utexas.edu/users/EWD/transcriptions/EWD13xx/EWD1300.html)
 --
 -- The precedence parser is based on Parsing expressions by precedence climbing
@@ -10,10 +11,14 @@
 -- http://www.engr.mun.ca/~theo/Misc/exp_parsing.htm
 -- http://antlr.org/papers/Clarke-expr-parsing-1986.pdf
 --
--- I added backtracking by expresion length with monadic parsers to solve ambiguities;
+-- I added backtracking by expresion length to solve ambiguities;
 --    by example, if we have to prefix operators:
 --    if_then_ and if_then_else, and the expresion if e0 then if e1 then e2 else e3, it is analised as
 --       the expresion if e0 (if e1 then e2 else e3)
+--
+-- Some examples are on the Examples.hs file
+-- 
+-- I developed this parser to construct a calculational style prover assistant.
 --
  
 module MixFix.Parser where
